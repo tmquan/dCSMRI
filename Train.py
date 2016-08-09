14aab9532f31558f6eb63a1b7eb66d8d19ebe6bf
tmquan <quantm@unist.ac.kr>
2016-08-10 02:14:24 +0900

    Cross validation 3 folds

--- Train.py
+++ Train.py
@@ -80,7 +80,7 @@ def train():
 			
 						
 			model.fit(X_train, y_train, 
-				run_id="fully_convolutional_neural_network", 
+				run_id="convolutional_autoencoder_network", 
 				n_epoch=1, 
 				validation_set=(X_valid, y_valid),
 				shuffle=False,
